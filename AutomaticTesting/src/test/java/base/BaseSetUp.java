98fb6ab8f54784be977e1eba3d9487c9063ece1d
ng0c4n <99899039+ng0c4n@users.noreply.github.com>
2023-07-18 00:31:11 +0700

    Update BaseSetUp.java

--- AutomaticTesting/src/test/java/base/BaseSetUp.java
+++ AutomaticTesting/src/test/java/base/BaseSetUp.java
@@ -26,7 +26,7 @@ public class BaseSetUp {
     public BaseSetUp(String webHref){
         //setup for web
         this.driverPath= "src/main/resources/chromedriver.exe";
-        this.webHref= "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";;
+        this.webHref= "http://www.demo.guru99.com/V4/";;
         initBrowserDriver();
        /* WebDriverWait wait = new WebDriverWait(driver,20); //20 seconds
         wait.until(ExpectedConditions.elementToBeClickable(element));
