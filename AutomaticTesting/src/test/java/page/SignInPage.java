47381a8a15946c5e9b4bca05ada1e7e8d0e5b23a
ng0c4n <99899039+ng0c4n@users.noreply.github.com>
2023-07-18 00:30:27 +0700

    Update SignInPage.java

--- AutomaticTesting/src/test/java/page/SignInPage.java
+++ AutomaticTesting/src/test/java/page/SignInPage.java
@@ -3,28 +3,28 @@ package page;
 import model.Authen;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import page.Page;
 
 public class SignInPage extends Page {
-    private Authen au;
+    private Authen authen;
 
-    public SignInPage(WebDriver driver, Authen au) {
+    public SignInPage(WebDriver driver, Authen authen) {
         super(driver);
-        this.au = au;
-    }
-    public boolean signin(String username, String password) throws Exception {
-        //get element user name
-        String befor= super.getDriver().getCurrentUrl();
-        WebElement ele_UserName= super.getElement(au.getEle_UserName());
-        //send element user name
-        super.sendKey(ele_UserName, username);
-        //get element pass word
-        WebElement ele_PassWord= super.getElement(au.getEle_PassWord());
-        //send element pass word
-        super.sendKey(ele_PassWord, password);
-        super.clickEvent(au.getEle_button());
-        String after= super.getDriver().getCurrentUrl();
-        return befor.equals(after)? false : true;
+        this.authen = authen;
     }
 
+    public boolean signin(String userid, String password) throws Exception {
+        WebElement ele_UserID = super.getElement(authen.getEle_UserID());
+        super.sendKey(ele_UserID, userid);
+
+        WebElement ele_PassWord = super.getElement(authen.getEle_PassWord());
+        super.sendKey(ele_PassWord, password);
+
+        WebElement ele_button = super.getElement(authen.getEle_button());
+        super.clickEvent(ele_button);
+
+        // Kiểm tra logic xử lý đăng nhập và trả về kết quả tương ứng
+        // ...
+
+        return true; // Hoặc false tùy theo kết quả kiểm tra
+    }
 }
